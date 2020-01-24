07923f23763ce6be5e57f81f6ef12632ab4c9c38
Orube Tecnologia <vinnicius60@hotmail.com>
2020-01-24 14:20:07 -0300

    aula 49

--- src/jogo/Perguntas.js
+++ src/jogo/Perguntas.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import {Grid, GridRow, GridColumn, Radio, Message, Button, CardContent, Container} from 'semantic-ui-react'
+import {Grid, GridRow, GridColumn, Radio, Message, Button, CardContent, Container, Icon, Progress} from 'semantic-ui-react'
 import axios from 'axios'
+import _ from 'lodash'
 
 import Header from './Header'
 
@@ -10,8 +11,12 @@ class Perguntas extends Component{
         super(props)
         this.state = {
             perguntas: {},
-            estaCarregando: false
-        }     
+            estaCarregando: false,
+            perguntaAtual: 0,
+            totalPerguntas: 0
+        }  
+        
+        this.proximaPergunta = this.proximaPergunta.bind(this)
 
     }
     componentDidMount(){
@@ -19,71 +24,122 @@ class Perguntas extends Component{
     }
 
     carregandoPerguntas(cat){
-        console.log('Categoria da pergunta ', cat)
+        console.log('Categoria da pergunta: ', cat)
         this.setState({
             estaCarregando: true,
             perguntas: {}
         })
 
-        const url = `https://projeto-react-653ae.firebaseio.com/categorias.json?orderBy="nome"&equalTo=${cat}`
+        const url = `https://projeto-react-653ae.firebaseio.com/categorias.json?orderBy="nome"&equalTo="${cat}"`
         axios
             .get(url)
             .then(dados => {
-                console.log('Lsita de perguntas', dados.data)
+                const chave = Object.keys(dados.data)[0]
+                const listaPerguntas = dados.data[chave]
+                console.log('Chaveeee', chave)
+                console.log('Lsita de perguntas', listaPerguntas)
+                console.log('Quantidade de perguntas: ', _.size(listaPerguntas) - 1)
+                this.setState({
+                    estaCarregando: false,
+                    perguntas: listaPerguntas,
+                    totalPerguntas: (_.size(listaPerguntas)-1)
+                })
             })
             .catch(err => {
                 console.log('Algum problema ocorreu')
             })
     }
+
+    proximaPergunta(){
+        const {perguntaAtual, totalPerguntas} = this.state
+        console.log(perguntaAtual, totalPerguntas)
+        if(perguntaAtual < totalPerguntas -1){
+            this.setState({perguntaAtual: this.state.perguntaAtual +1})
+
+        }else{
+            console.log('Terminou as perguntas, mostre as respostas')
+        }
+
+    }
+
+    renderPergunta(pergunta){
+        return (
+            <span >
+                <h3>PERGUNTA:{pergunta.titulo}</h3>
+                {console.log('aquiiii', pergunta)}
+                <Grid columns={2}>
+                <GridRow>              
+
+                    <GridColumn>
+                        <Message color='blue'>
+                            
+                            <p>{pergunta.alternativas[1].resposta}</p>                                
+                            <Radio toggle/>                            
+                        </Message>                        
+
+                    </GridColumn>
+                    <GridColumn>
+                        <Message color='pink'>
+                            <p>{pergunta.alternativas[2].resposta}</p>                                
+                            <Radio toggle/>                            
+                        </Message>  
+                    </GridColumn>
+                </GridRow>
+
+                <GridRow>
+                    <GridColumn>
+                        <Message color='orange'>
+                            <p>{pergunta.alternativas[3].resposta}</p>                                
+                            <Radio toggle/>                            
+                        </Message>  
+                    </GridColumn>
+                    <GridColumn>
+                        <Message color='green'>
+                            <p>{pergunta.alternativas[4].resposta}</p>                                
+                            <Radio toggle/>                            
+                        </Message>  
+                    </GridColumn>                        
+                </GridRow>
+            </Grid>
+        </span>
+        )
+    }
     
     
     render(){
+
+        let item = []
+        if(this.state.estaCarregando){
+            return <p>Carregando...</p>
+        }
+
         return(
             <div>
-                <Header />                
-                <h2>{this.props.match.params.nome}</h2>
+                <Header /> 
+                              
+                <h2><Icon name={this.state.perguntas.icone}/> {this.props.match.params.nome}</h2>
                 <p>Mostre que você conhece tudo sobre esse assunto</p>
-                <h3>PERGUNTA1: Qual a profissão que...</h3>
-    
-                <Grid columns={2}>
-                    <GridRow>
-                        <GridColumn>
-                            <Message color='blue'>
-                                <p>Alternativa 1</p>                                
-                                <Radio toggle/>                            
-                            </Message>                        
-    
-                        </GridColumn>
-                        <GridColumn>
-                            <Message color='pink'>
-                                <p>Alternativa 2</p>                                
-                                <Radio toggle/>                            
-                            </Message>  
-                        </GridColumn>
-                    </GridRow>
-    
-                    <GridRow>
-                        <GridColumn>
-                            <Message color='orange'>
-                                <p>Alternativa 3</p>                                
-                                <Radio toggle/>                            
-                            </Message>  
-                        </GridColumn>
-                        <GridColumn>
-                            <Message color='green'>
-                                <p>Alternativa 4</p>                                
-                                <Radio toggle/>                            
-                            </Message>  
-                        </GridColumn>                        
-                    </GridRow>
+                
+                {
 
-                    <Container>
-                        <CardContent>
-                            <Button basic color='blue' >Finalizar</Button>
-                        </CardContent>
-                    </Container>
-    
-                </Grid>
+                    this.state.perguntas.perguntas && Object.keys(this.state.perguntas.perguntas)
+                    .map(key =>{
+                        item.push(key)
+                    })
+                }
+                {
+                    this.state.perguntas.perguntas && this.renderPergunta(this.state.perguntas.perguntas[item[this.state.perguntaAtual]])
+
+                    
+                }
+                
+                <Progress value={this.state.perguntaAtual + 1} total= {item.length} progress='ratio'/>
+                <Container>
+                    <CardContent>
+                        <Button basic color='blue' onClick={this.proximaPergunta}>Próximo</Button>
+                    </CardContent>
+                </Container>   
+             
             </div>    
         )
     }
